0c26d6c032226f8445be45230336f86d2eaffe30
dairansc <dairansc@gmail.com>
2012-06-08 00:10:51 +0000

    atualizações da biblioteca

--- codigos/biblioteca.c
+++ codigos/biblioteca.c
@@ -1,6 +1,14 @@
 #define BUFFERMAX 1500
 #define PORTAPADRAO 10000
 #define NOMEARQUIVOMAX BUFFERMAX-sizeof(long int)-sizeof(int)
+#define TAMDADOSMAX BUFFERMAX-sizeof(struct datagramHeader)
+#define TAMJANELA 10
+
+// indicadores de conexão
+#define SYN   0x01
+#define FIM   0x02
+#define ACK   0x04
+#define NACK  0x08
 
 struct fileHeader
 {
@@ -8,6 +16,17 @@ struct fileHeader
     int tamanhoNome;
     char nome[NOMEARQUIVOMAX];
 };
+
+struct datagramHeader
+{
+    short flags;            // indicadores de conexão
+    long int sequencia;
+    long int idRecebido;    // Acknowlegement
+    int janela;             // tamanho da janela
+};
+
+
+
 
 char *nomeBaseArquivo(char *caminho)
 {
