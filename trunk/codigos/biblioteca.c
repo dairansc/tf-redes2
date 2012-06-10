80b2aaec88daff38fa26d9c6491dd407cbcf64ae
dairansc@gmail.com <dairansc@gmail.com@74243c3c-7e40-100c-db53-f22f8c76de9a>
2012-06-10 17:35:05 +0000

    modificações envianado pacotes em janela, e com funções para enviar e receber pacotes

--- trunk/codigos/biblioteca.c
+++ trunk/codigos/biblioteca.c
@@ -1,7 +1,8 @@
 #define BUFFERMAX 1500
 #define PORTAPADRAO 10000
-#define NOMEARQUIVOMAX BUFFERMAX-sizeof(long int)-sizeof(int)
-#define TAMDADOSMAX BUFFERMAX-sizeof(struct datagramHeader)
+
+#define TAMDADOSMAX BUFFERMAX-sizeof(short)-sizeof(long int)-sizeof(long int)-sizeof(int)
+#define NOMEARQUIVOMAX TAMDADOSMAX-sizeof(long int)
 #define TAMJANELA 10
 
 // indicadores de conexão
@@ -10,10 +11,11 @@
 #define ACK   0x04
 #define NACK  0x08
 
+
+
 struct fileHeader
 {
     long int tamanho;
-    int tamanhoNome;
     char nome[NOMEARQUIVOMAX];
 };
 
@@ -23,10 +25,68 @@ struct datagramHeader
     long int sequencia;
     long int idRecebido;    // Acknowlegement
     int janela;             // tamanho da janela
+    char dados[TAMDADOSMAX];
 };
 
 
+int Sock; // descritor de Socket
+struct sockaddr_in ToAddr; // Endereço destino
+struct sockaddr_in FromAddr; // Endereço origem
+unsigned short Porta;     // Porta aberta de conexão
+FILE *Arquivo;              // Ponteiro para o arquivo
+short Reenviar;             // Indica se janela precisa ser reenviada
+char BufferJanela[TAMDADOSMAX*TAMJANELA];
+int ContJanela;
 
+void configuraPorta(char *parametro)
+{
+    if (!parametro || !atoi(parametro))
+    {
+        printf("Atenção: Porta não definida, assumindo porta padrão '%d'.\n", PORTAPADRAO);
+        Porta = PORTAPADRAO;
+    }
+    else
+    {
+        Porta = atoi(parametro);
+    }
+}
+
+void enviaPacote(struct datagramHeader pacote)
+{
+    char buffer[BUFFERMAX];
+
+    printf("%d\n\n", pacote.janela);
+
+    memset(buffer, 0, sizeof(buffer));
+    memcpy(buffer, &(pacote), sizeof(struct datagramHeader));
+
+    if (sendto(Sock, buffer, BUFFERMAX, 0, (struct sockaddr *) &ToAddr, sizeof(ToAddr)) != BUFFERMAX)
+        DieWithError("sendto() sent a different number of bytes than expected");
+
+}
+
+struct datagramHeader *recebePacote(short verificarOrigem)
+{
+    char buffer[BUFFERMAX];
+    unsigned int fromSize = sizeof(FromAddr);
+
+    memset(buffer, 0, sizeof(buffer));
+
+    // Recebe resposta
+    memset(buffer, 0, sizeof(buffer));
+    if ((recvfrom(Sock, buffer, BUFFERMAX, 0, (struct sockaddr *) &FromAddr, &fromSize)) != BUFFERMAX)
+        DieWithError("recvfrom() failed");
+
+
+    if (verificarOrigem && ToAddr.sin_addr.s_addr != FromAddr.sin_addr.s_addr)
+    {
+        fprintf(stderr,"Error: received a packet from unknown source.\n");
+        exit(1);
+    }
+
+    return (struct datagramHeader *)buffer;
+
+}
 
 char *nomeBaseArquivo(char *caminho)
 {
